883ce4988ae1405c342c64546b9313ce8ecb5251
Andrey Tsepkov <cepkov78@mail.ru>
2020-02-12 14:04:16 +0600

    Add generic IRepository

---
+++ SMS.Domain/Abstract/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMS.Domain.Abstract
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        IEnumerable<T> Find(Func<T, bool> predicate);
+        void Create(T item);
+        void Update(T item);
+        //void Delete(int id);
+        void Delete(T item);
+    }
+}
