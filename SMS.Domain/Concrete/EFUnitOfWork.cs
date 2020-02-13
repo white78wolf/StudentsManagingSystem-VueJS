c35522900f282567e92c2535ae478d17a424ec47
Andrey Tsepkov <cepkov78@mail.ru>
2020-02-13 20:37:20 +0600

    Add EFUnitOfWork and change StudentsRepo inheriting it from IRepository for that case

---
+++ SMS.Domain/Concrete/EFUnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using SMS.Domain.Abstract;
+using SMS.Domain.Entities;
+using System;
+
+namespace SMS.Domain.Concrete
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private StudentsContext db;
+        private StudentsRepository studentsRepository;
+
+        public EFUnitOfWork(StudentsContext context)
+        {
+            db = context;
+        }
+
+        public IRepository<Student> Students
+        {
+            get
+            {
+                if (studentsRepository == null)
+                    studentsRepository = new StudentsRepository(db);
+                return studentsRepository;
+            }
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- SMS.Domain/Concrete/StudentsRepository.cs
+++ SMS.Domain/Concrete/StudentsRepository.cs
@@ -1,47 +1,94 @@
 ﻿using SMS.Domain.Abstract;
 using SMS.Domain.Entities;
+using System;
+using System.Linq;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace SMS.Domain.Concrete
 {
-    public class StudentsRepository: IStudentsRepository
+    public class StudentsRepository: IRepository<Student>//IStudentsRepository
     {
-        StudentsContext _context;
+        //IEnumerable<T> GetAll();
+        //T Get(int id);
+        //IEnumerable<T> Find(Func<T, bool> predicate);
+        //void Create(T item);
+        //void Update(T item);
+        //void Delete(T item);
+
+        private StudentsContext db;
 
         public StudentsRepository(StudentsContext context)
         {
-            _context = context;
+            db = context;
         }
 
-        public IEnumerable<Student> Students => _context.Students;
-
-        public void SaveStudent(Student student)
+        public IEnumerable<Student> GetAll()
         {
-            if (student.Id == 0)
-                _context.Students.Add(student);
-            else
-            {
-                Student dbEntry = _context.Students.Find(student.Id);
-                if (dbEntry != null)
-                {
-                    dbEntry.UniqId = student.UniqId;
-                    dbEntry.Name = student.Name;
-                    dbEntry.MiddleName = student.MiddleName;
-                    dbEntry.LastName = student.LastName;
-                    dbEntry.Gender = student.Gender;
-                }
-            }
-            _context.SaveChanges();
+            return db.Students;
         }
-        
-        public Student DeleteStudent(Student student)
-        {            
-            if (student != null)
-            {
-                _context.Students.Remove(student);
-                _context.SaveChanges();
-            }
-            return student;
+
+        public Student Get(int id)
+        {
+            return db.Students.Find(id);
+        }
+
+        public IEnumerable<Student> Find(Func<Student, bool> predicate)
+        {
+            return db.Students.Where(predicate).ToList();
+        }
+
+        public void Create(Student student)
+        {
+            db.Add(student);
+        }
+
+        public void Update(Student student)
+        {
+            db.Entry(student).State = EntityState.Modified;
         }        
+
+        public void Delete(Student student)
+        {
+            db.Remove(student);
+        }
+
+        //StudentsContext _context;
+
+        //public StudentsRepository(StudentsContext context)
+        //{
+        //    _context = context;
+        //}
+
+        //public IEnumerable<Student> Students => _context.Students;
+
+        //public void SaveStudent(Student student)
+        //{
+        //    if (student.Id == 0)
+        //        _context.Students.Add(student);
+        //    else
+        //    {
+        //        Student dbEntry = _context.Students.Find(student.Id);
+        //        if (dbEntry != null)
+        //        {
+        //            dbEntry.UniqId = student.UniqId;
+        //            dbEntry.Name = student.Name;
+        //            dbEntry.MiddleName = student.MiddleName;
+        //            dbEntry.LastName = student.LastName;
+        //            dbEntry.Gender = student.Gender;
+        //        }
+        //    }
+        //    _context.SaveChanges();
+        //}
+
+        //public Student DeleteStudent(Student student)
+        //{            
+        //    if (student != null)
+        //    {
+        //        _context.Students.Remove(student);
+        //        _context.SaveChanges();
+        //    }
+        //    return student;
+        //}        
     }
 }
