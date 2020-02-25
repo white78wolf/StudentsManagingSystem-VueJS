b64ddfdb32c81527f9b9dd1551701314f29f29ed
Andrey Tsepkov <cepkov78@mail.ru>
2020-02-25 16:54:45 +0600

    Clean StudentsController.

--- StudentsManagingSystem/Controllers/StudentsController.cs
+++ StudentsManagingSystem/Controllers/StudentsController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SMS.Domain.Abstract;
 using SMS.Domain.Entities;
@@ -7,38 +6,33 @@ namespace StudentsManagingSystem.Controllers
 {
     [Route("api/[controller]")]    
     public class StudentsController : Controller
-    {
-        //private IRepository _repository;
+    {        
         private IUnitOfWork _uow;
 
         public StudentsController(IUnitOfWork uow)
-        {
-
-            //_repository = repository;
+        {            
             _uow = uow;
 
             // initializing the table by fake students
             if (_uow.Students == null)
-            {
-                //_repository.SaveStudent(new Student { Name = "Роман", LastName = "Ойра-Ойра", Gender = "male" });
-                //_repository.SaveStudent(new Student { Name = "Александр", LastName = "Привалов", Gender = "male" });
-                //_repository.SaveStudent(new Student { Name = "Ольга", LastName = "Онучкина", Gender = "female" });
-                //_repository.SaveStudent(new Student { Name = "Стелла", LastName = "Иванова", Gender = "female" });                
+            {                
+                _uow.Students.Create(new Student { Name = "Роман",     LastName = "Ойра-Ойра", Gender = "male"   });
+                _uow.Students.Create(new Student { Name = "Александр", LastName = "Привалов",  Gender = "male"   });
+                _uow.Students.Create(new Student { Name = "Ольга",     LastName = "Онучкина",  Gender = "female" });
+                _uow.Students.Create(new Student { Name = "Стелла",    LastName = "Иванова",   Gender = "female" }); 
             }
         }        
 
         [HttpGet]
         public IEnumerable<Student> Get()
-        {
-            //return _repository.Students.ToList();
+        {            
             return _uow.Students.GetAll();
         }
 
         // method to fill up client's form by response, using student's id
         [HttpGet("{id:int}")]
         public IActionResult Get(int id)
-        {
-            //Student student = _repository.Students.FirstOrDefault(x => x.Id == id);            
+        {            
             Student student = _uow.Students.Get(id);
             if (student == null)
                 return NotFound();
