1b36eaec84f600b0e05060505c4deb9d7934c3dc
Andrey Tsepkov <cepkov78@mail.ru>
2020-02-25 17:30:50 +0600

    Clean StudentsRepository from commented old code.

--- SMS.Domain/Concrete/StudentsRepository.cs
+++ SMS.Domain/Concrete/StudentsRepository.cs
@@ -7,15 +7,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace SMS.Domain.Concrete
 {
-    public class StudentsRepository: IRepository<Student>//IStudentsRepository
+    public class StudentsRepository: IRepository<Student>
     {
-        //IEnumerable<T> GetAll();
-        //T Get(int id);
-        //IEnumerable<T> Find(Func<T, bool> predicate);
-        //void Create(T item);
-        //void Update(T item);
-        //void Delete(T item);
-
         private StudentsContext db;
 
         public StudentsRepository(StudentsContext context)
@@ -51,44 +44,6 @@ namespace SMS.Domain.Concrete
         public void Delete(Student student)
         {
             db.Remove(student);
-        }
-
-        //StudentsContext _context;
-
-        //public StudentsRepository(StudentsContext context)
-        //{
-        //    _context = context;
-        //}
-
-        //public IEnumerable<Student> Students => _context.Students;
-
-        //public void SaveStudent(Student student)
-        //{
-        //    if (student.Id == 0)
-        //        _context.Students.Add(student);
-        //    else
-        //    {
-        //        Student dbEntry = _context.Students.Find(student.Id);
-        //        if (dbEntry != null)
-        //        {
-        //            dbEntry.UniqId = student.UniqId;
-        //            dbEntry.Name = student.Name;
-        //            dbEntry.MiddleName = student.MiddleName;
-        //            dbEntry.LastName = student.LastName;
-        //            dbEntry.Gender = student.Gender;
-        //        }
-        //    }
-        //    _context.SaveChanges();
-        //}
-
-        //public Student DeleteStudent(Student student)
-        //{            
-        //    if (student != null)
-        //    {
-        //        _context.Students.Remove(student);
-        //        _context.SaveChanges();
-        //    }
-        //    return student;
-        //}        
+        }       
     }
 }
